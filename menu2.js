8a1796d019e657cbadf4eac70730e414cb1801f6
DaSemilla94 <mathieu.kauffmann06@gmail.com>
2025-03-24 11:46:49 +0100

    menu2.js

--- menu2.js
+++ menu2.js
@@ -1,62 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.menu-toggle');
-    const navUl = document.querySelector('nav ul');
+    const nav = document.querySelector('.nav');
 
-    function slideDown(element, duration) {
-        element.style.display = 'flex';
-        const height = element.scrollHeight;
-        element.style.maxHeight = '0';
-        element.style.overflow ='hidden';
-
-        let start = null;
-        function animate(timestamp) {
-            if (!start) start = timestamp;
-            const progress = timestamp - start;
-            const percent = Math.min(progress / duration, 1);
-            element.style.maxHeight = `${percent * height}px`;
-
-            if (progress < duration) {
-                requestAnimationFrame(animate);
-            }
-        }
-        requestAnimationFrame(animate)
-    }
-
-    function slideUp(element, duration) {
-        const height = element.scrollHeight;
-        element.style.maxHeight = `${height}px`;
-
-        let start = null;
-        function animate(timestamp) {
-            if (!start) start = timestamp;
-            const progress = timestamp - start;
-            const percent = 1 - Math.min(progress / duration, 1);
-            element.style.maxHeight = `${percent * height}px`;
-
-            if (progress < duration) {
-                requestAnimationFrame(animate);
-            } else {
-                element.style.display = 'none';
-            } 
-        }
-        requestAnimationFrame(animate);
-    }
-
-    menuToggle.addEventListener('click', function () {
-        if (navUl.classList.contains('active')) {
-            slideUp(navUl, 300);
-            navUl.classList.remove('active');
-        } else {
-            slideDown(navUl, 300);
-            navUl.classList.add('active');
-        }
+    menuToggle.addEventListener("click", function() {
+        nav.classList.toggle("active");
     });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    let element = document.querySelector(".page");
-    element.classList.add("slide-in-right");
-});
-document.querySelector(".menu-toggle").addEventListener("click", function() {
-    document.querySelector(".nav").classlist.toggle("active");
 });
