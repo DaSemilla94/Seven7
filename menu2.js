5d204703d0781ee48d0237dff87eed801bae4b17
DaSemilla94 <mathieu.kauffmann06@gmail.com>
2025-03-26 11:26:31 +0100

    menu2.js

--- menu2.js
+++ menu2.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.menu-toggle');
     const nav = document.querySelector('.nav');
 
-    if (menToggle && nav) {
+    if (menuToggle && nav) {
         menuToggle.addEventListener("click", function() {
             nav.classList.toggle("active");
         });
